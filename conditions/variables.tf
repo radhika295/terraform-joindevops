94cd70b1cdd2aab3470efcdc5bf87c1f33776c0d
Radhika <sairadhika.iiitn@gmail.com>
2025-10-15 23:10:52 +0530

    conditions

---
+++ conditions/variables.tf
@@ -0,0 +1,16 @@
+variable instance_type {
+    default = "t3.micro"
+}
+
+variable instance_name {
+
+    default = ["mongodb","mysql","reddis","rabbitmq"]
+}
+
+variable zone_id {
+    default = "Z07503961RHS7OI25U00O"
+}
+
+variable domain_name {
+    default = "dev.training.icu"
+}
